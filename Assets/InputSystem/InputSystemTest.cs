04a562f5908521603c33e4e49c2b8b44f24f3de7
Usagi <kirito232k@gmail.com>
2026-01-07 10:22:40 +0200

    Add force movement

--- Assets/InputSystem/InputSystemTest.cs
+++ Assets/InputSystem/InputSystemTest.cs
@@ -9,8 +9,12 @@ namespace InputSystem
     {
         [SerializeField] private InputActionReference jumpAction;
         [SerializeField] private InputActionReference moveAction;
-        [SerializeField] private float baseSpeed = 10.0f;
+        [SerializeField] private float moveSpeed = 10f;
+        [SerializeField] private float maxSpeed = 100f;
+        [SerializeField] private float jumpForce = 5f;
+        
         private Rigidbody _rb;
+        private Vector2 _moveInput;
 
         private void Awake()
         {
@@ -20,21 +24,63 @@ namespace InputSystem
 
         private void Update()
         {
-            HandleMovement();
+            HandleInputs();
             HandleJump();
+        }
+
+        private void FixedUpdate()
+        {
+            HandleMovement();
+        }
+
+        private void HandleInputs()
+        {
+            _moveInput = moveAction.action.ReadValue<Vector2>();
         }
 
         private void HandleMovement()
         {
-           Vector2 moveDirection = moveAction.action.ReadValue<Vector2>();
-           if (moveDirection == Vector2.zero) return;
+            if (_moveInput == Vector2.zero) return;
+            //movePositionMove();
+            addForceMove();
+        }
 
-           _rb.MovePosition(_rb.position + (new Vector3(moveDirection.x, 0f, moveDirection.y))*baseSpeed * Time.deltaTime);
+        private void movePositionMove()
+        {
+            _rb.MovePosition(_rb.position
+                             + new Vector3(_moveInput.x, 0f, _moveInput.y)
+                             * moveSpeed
+                             * Time.fixedDeltaTime);
+        }
+
+        private void addForceMove()
+        {
+            Vector3 inputDir =
+                new Vector3(_moveInput.x, 0f, _moveInput.y);
+
+            // if (_rb.linearVelocity.magnitude < maxSpeed-5f)
+            // {
+            //     moveSpeed*=2f;
+            // }
+            
+            Vector3 desiredAcceleration = inputDir * moveSpeed;
+            _rb.AddForce(desiredAcceleration, ForceMode.Acceleration);
+
+            Vector3 velocity = _rb.linearVelocity;
+            Vector3 horizontal = new Vector3(velocity.x, 0f, velocity.z);
+            
+            
+            if (horizontal.magnitude > maxSpeed)
+            {
+                Vector3 limited = horizontal.normalized * maxSpeed;
+                _rb.linearVelocity = new Vector3(limited.x, velocity.y, limited.z);
+            }
         }
         
         private void HandleJump()
         {
             if (!jumpAction.action.triggered) return;
+            _rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
         }
     }
 }
