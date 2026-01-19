77858718e1fe4569d413bd88233cce5282a26a8d
Usagi <kirito232k@gmail.com>
2026-01-19 18:41:41 +0200

    IsPlayerGrounded?

--- Assets/Scripts/InputSystem/ThirdPersonController.cs
+++ Assets/Scripts/InputSystem/ThirdPersonController.cs
@@ -8,21 +8,24 @@ namespace InputSystem
 {
     public class ThirdPersonController : MonoBehaviour
     {
-        [Header("Actions")] 
+        [Header("Actions")]
         [SerializeField] private InputActionReference moveAction;
         [SerializeField] private InputActionReference lookAction;
         [SerializeField] private InputActionReference jumpAction;
 
-        [Header("Params")] 
+        [Header("Params")]
         [SerializeField] private float moveSpeed = 10f;
         [SerializeField] private float maxSpeed = 10f;
         [SerializeField] private float jumpForce = 5f;
 
-        [Header("Camera")] 
-        [SerializeField] private Transform cameraTransform;
-        //[SerializeField] private bool lookMoveDirection = true;
-        
-        [Header("Aim/Camera Pitch")]
+        [Header("Ground Detection")]
+        [SerializeField] private Transform groundCheckSphereCenter;
+        [SerializeField] private float groundRadius;
+        //[SerializeField] private float maxFallingSpeed = -1f;
+        [SerializeField] private LayerMask whatIsGround;
+        [SerializeField] private bool isGrounded;
+
+        [Header("Aim/Camera Pitch")] 
         [SerializeField] private Transform cameraPitchTarget;
         [SerializeField] private float pitchSensitivity = 0.1f;
         [SerializeField] private float mouseSensitivity = 0.1f;
@@ -30,7 +33,12 @@ namespace InputSystem
         [SerializeField] private float pitchMin = -20f;
         [SerializeField] private float pitchMax = 60f;
         [SerializeField] private LayerMask aimCollisionLayerMask;
-        
+
+        [Header("Camera")]
+        [SerializeField] private Transform cameraTransform;
+        //[SerializeField] private bool lookMoveDirection = true;
+
+
         private Vector2 lookDelta = Vector2.zero;
         private float _pitch;
 
@@ -48,22 +56,25 @@ namespace InputSystem
 
         private void Update()
         {
+            HandleIsGrounded(); //for update mid air. Is this correct? Or call in HandleJump action?
+                
             HandleInputs();
             HandleCameraDirection();
             HandleJump();
-            
+
             RotateFromLookAction();
             RotateCameraPitchFromLook();
         }
+
         private void FixedUpdate()
         {
             HandleMovement();
         }
-        
+
         private void HandleInputs()
         {
             lookDelta = lookAction.action.ReadValue<Vector2>();
-            
+
             if (moveAction?.action != null) _moveInput = moveAction.action.ReadValue<Vector2>();
         }
 
@@ -72,9 +83,9 @@ namespace InputSystem
             Vector3 forward = cameraTransform.forward;
             Vector3 right = cameraTransform.right;
 
-            forward.y = 0; 
+            forward.y = 0;
             right.y = 0;
-            forward.Normalize(); 
+            forward.Normalize();
             right.Normalize();
 
             _moveDirection = forward * _moveInput.y + right * _moveInput.x;
@@ -107,7 +118,7 @@ namespace InputSystem
             Quaternion targetRotation = Quaternion.Euler(0f, _yaw, 0f);
             _rb.MoveRotation(targetRotation);
         }
-        
+
         private void RotateCameraPitchFromLook()
         {
             float deltaPitch = lookDelta.y * pitchSensitivity * 0.1f;
@@ -118,13 +129,26 @@ namespace InputSystem
             euler.x = _pitch;
             cameraPitchTarget.localEulerAngles = euler;
         }
-        
+
         private void HandleJump()
         {
-            if (jumpAction.action.triggered) //TODO isGrounded check
+            if (jumpAction.action.triggered && isGrounded) // && HandleIsGrounded() because doesnt update mid air
             {
                 _rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
             }
+        }
+
+        private bool HandleIsGrounded()
+        {
+            //----DO NOT WORK BECAUSE OF rb.velocity value on slopes----
+            // Debug.Log(_rb.linearVelocity.y);
+            // if(_rb.linearVelocity.y <= maxFallingSpeed)
+            // {
+            //     isGrounded = false;
+            //     return false; // cancel jump mid air
+            // }
+            isGrounded = Physics.CheckSphere(groundCheckSphereCenter.position, groundRadius, aimCollisionLayerMask);
+            return isGrounded;
         }
     }
 }
